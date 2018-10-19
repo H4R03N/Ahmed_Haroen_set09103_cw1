34c848b46601b99c85e817e4034294e383f903b1
H4R03N <40403712@live.napier.ac.uk>
2018-10-20 00:52:43 +0100

    active

--- routes.py
+++ routes.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', active='index')
 
 @app.route('/habitat')
 def habitat():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 	with open(os.path.join(SITE_ROOT,'static', 'data.json')) as f:
 		data = json.load (f)
-        	return render_template('habitat.html')
+        	return render_template('habitat.html', active='habitat')
 
 @app.route('/reptiles')
 def reptiles():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 	with open(os.path.join(SITE_ROOT,'static', 'data.json')) as f:
 		data = json.load (f)
-		return render_template ('reptiles.html', data=data)
+		return render_template ('reptiles.html', active='reptiles',  data=data)
 
 
 @app.route('/mammals')	
@@ -32,7 +32,7 @@ def mammals():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 	with open (os.path.join(SITE_ROOT, 'static', 'data.json')) as f:
 		data = json.load (f)
-		return render_template ('mammals.html', data=data)
+		return render_template ('mammals.html', active='mammals', data=data)
 
 
 @app.route('/birds')
@@ -40,12 +40,12 @@ def birds():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 	with open (os.path.join(SITE_ROOT, 'static', 'data.json')) as f:
 		data = json.load (f)
-		return render_template ('birds.html', data=data)
+		return render_template ('birds.html', active='birds', data=data)
 
 @app.route('/amphibian')
 def amphibian():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 	with open (os.path.join(SITE_ROOT, 'static', 'data.json')) as f:
 		data = json.load (f)
-		return render_template ('amphibian.html', data=data)
+		return render_template ('amphibian.html', active='amphibian', data=data)
 
